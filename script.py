22d7dc9e6ab48a705e6946b0bb90475eb024593a
Akil Ismail <12010232@students.liu.edu.lb>
2025-02-21 02:46:44 +0200

    required outputs

--- script.py
+++ script.py
@@ -12,3 +12,12 @@ rent=int(input("\t • rent : "))
 
 # input electricity:
 electricity=int(input("\t • electricity : ")) 
+
+#display amount allocated to savings, rent, and electricity.
+#display total amount spent on savings, rent, and electricity combined.
+#display The remainder of the salary after these expenses.
+#display The monthly rent and electricity multiplied by 12 to estimate yearly rent and electricity costs.
+#display total salary for the month raised to the power of 2 (just for fun).
+#display Assume we save an additional random amount (e.g., $50) each month, calculate how much would be left if this amount is divided by the total amount allocated to savings. 
+
+#Finally, the script should output all the results in a readable format.(>>>>> JUST A REMARK <<<<<<)
