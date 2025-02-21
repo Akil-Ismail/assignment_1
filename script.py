dd581048109dd8082805b8046aac71b8a2bbf9f8
Akil Ismail <12010232@students.liu.edu.lb>
2025-02-21 02:42:19 +0200

    Nabiha's inputs

--- script.py
+++ script.py
@@ -1,5 +1,14 @@
-# input salary for the month
-# input the name of the month you are storing the salary for
-# input savings
-# input rent
-# input electricity
+# input salary for the month:
+salary=int(input("please enter your salary for the month : ")) 
+
+# input the name of the month you are storing the salary for:
+month=str(input("please enter the name of the month you are storing the salary for : "))
+
+# input savings:
+savings=int(input("please enter the following percentages : \n \t • savings : ")) 
+
+# input rent:
+rent=int(input("\t • rent : "))
+
+# input electricity:
+electricity=int(input("\t • electricity : ")) 
